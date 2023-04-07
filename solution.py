489f07ccb22f2f19d11b20503587d0fc8b3bd182
catinabox1 <71033355+catinabox1@users.noreply.github.com>
2023-04-07 23:27:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,9 +7,12 @@ from scipy.stats import norm
 chat_id = 417796486 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-alpha = 1 - p
-n = len(x)
-left = (-min(-x) - 1 / 2) / (14**2 / 2)
-right = (-np.log(alpha) / n -min(-x) - 1 / 2) / (14**2 / 2)
-return left, \
-right
+  
+t = 29
+loc = sum(x)/len(x)
+
+z_1 = st.gamma.ppf((1+p)/2, a = len(x), scale = 1/len(x))
+z_2 = st.gamma.ppf((1-p)/2, a = len(x), scale = 1/len(x))
+
+ return 2*loc/(t**2) - 2/(2*t**2) + 2*z_2/(t**2), \
+        2*loc/(t**2) - 2/(2*t**2) + 2*z_1/(t**2)
